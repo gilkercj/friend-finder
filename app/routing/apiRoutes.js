aa6b37848092850469345d5ad01fadd79c3e1957
Charles Gilker <charlesjgilker@gmail.com>
2018-04-30 22:59:18 -0400

    final commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 
 
@@ -11,9 +11,32 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
 
+        var bestFriend = {
+            name: "",
+            photo: "",
+            dif: 100,
+        };
+
+
         var newFriend = req.body;
-        console.log(newFriend);
+        var dif = 0;
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i])
+            dif = 0
+            for (var j = 0; j < friends[i].scores[j].length; j++) {
+                dif = Math.abs(parseInt(newFriend.scores[j] - friends[i].scores[j]));
+                console.log(friends[i]);
+                if (dif < bestFriend.dif) {
+                    bestFriend.name = friends[i].name;
+                    bestFriend.photo = friends[i].photo;
+                    bestFriend.dif = dif;
+                }
+            }
+        }
+        res.json(bestFriend);
         friends.push(newFriend);
+        console.log(bestFriend);
+
 
     });
 }
--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "application/*+json"}));
+app.use(bodyParser.raw({ type: "application/vnd.custom-type"}));
+
+app.use(bodyParser.text({ type: 'text/html'}))
 
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 
 
