7dd804e411dc22f7d00f63d2bb7963845e44a8ab
Charles Gilker <charlesjgilker@gmail.com>
2018-04-30 21:29:20 -0400

    deploy update commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,6 +11,9 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
 
-        friends.push(req.body);
+        var newFriend = req.body;
+        console.log(newFriend);
+        friends.push(newFriend);
+
     });
 }
